371deb8c2cedede68bcfb8b69ecee64b46886353
Kwan <allenxian@live.com>
2019-02-17 15:03:54 +0800

    Initial commit

--- GPIO/LED_demo/one_led.py
+++ GPIO/LED_demo/one_led.py
@@ -8,7 +8,6 @@ from time import sleep
 def gpio_init():
     GPIO.setboard(GPIO.PCPCPLUS)
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(12, GPIO.OUT)
     GPIO.setup(16, GPIO.OUT)
     print("GPIO init success!")
 
@@ -16,15 +15,12 @@ def gpio_init():
 if __name__ == '__main__':
     try:
         gpio_init()
-        GPIO.output(12, 1)
-        GPIO.output(16, 1)
-        sleep(5)
-        GPIO.output(12, 0)
-        GPIO.output(16, 0)
-        # while True:
-        #     GPIO.output(12, 1)
-        #     sleep(1)
-        #     GPIO.output(12, 0)
+        while True:
+            GPIO.output(16, 1)
+            sleep(1)
+            GPIO.output(16, 0)
+    except KeyboardInterrupt:
+        exit(1)
     except Exception as err:
         print(err)
     finally:
