e7f96b984c6c3cec230ed362f03683d5e3d1dc60
Kwan <allenxian@live.com>
2019-02-17 16:10:38 +0800

    Initial commit

--- GPIO/Input_demo/button_event.py
+++ GPIO/Input_demo/button_event.py
@@ -35,8 +35,7 @@ if __name__ == '__main__':
         # 事件触发方式等待
         GPIO.add_event_detect(18, GPIO.RISING, callback=button_event, bouncetime=200)
         # 子线程等待
-        t1 = Thread(target=long_time_thread)
-        t1.start()
+        Thread(target=long_time_thread).start()
     except KeyboardInterrupt:
         print("stop")
     except Exception as err:
