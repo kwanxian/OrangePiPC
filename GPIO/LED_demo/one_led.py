1b8fd305dd047782cfb9d0375084054e45bdc664
Kwan <allenxian@live.com>
2019-02-17 15:27:27 +0800

    Initial commit

--- GPIO/LED_demo/one_led.py
+++ GPIO/LED_demo/one_led.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # encoding: utf-8
 
+# 利用GPIO输出3.3v点亮LED，每一秒闪一下
 import OPi.GPIO as GPIO
 from time import sleep
 
@@ -8,7 +9,6 @@ from time import sleep
 def gpio_init():
     GPIO.setboard(GPIO.PCPCPLUS)
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(12, GPIO.OUT)
     GPIO.setup(16, GPIO.OUT)
     print("GPIO init success!")
 
@@ -18,15 +18,12 @@ if __name__ == '__main__':
         gpio_init()
         print("Press Ctrl + C to Stop!")
         while True:
-            GPIO.output(12, 1)
             GPIO.output(16, 1)
             sleep(1)
-            GPIO.output(12, 0)
             GPIO.output(16, 0)
             sleep(1)
     except KeyboardInterrupt:
         print("Stop")
-        exit(1)
     except Exception as err:
         print(err)
     finally:
