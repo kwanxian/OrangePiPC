dd73c8452459d25eac67b6216b43afcc35b88d61
Kwan <allenxian@live.com>
2019-02-17 14:28:34 +0800

    Initial commit

--- GPIO/LED_demo/one_led.py
+++ GPIO/LED_demo/one_led.py
@@ -2,15 +2,22 @@
 # encoding: utf-8
 
 import OPi.GPIO as GPIO
+from time import sleep
 
 
 def gpio_init():
-    pass
+    GPIO.setboard(GPIO.PCPLUS)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(12, GPIO.OUT, pull_up_down=GPIO.PUD_UP)
 
 
 if __name__ == '__main__':
     try:
         gpio_init()
+        while True:
+            GPIO.output(12, 1)
+            sleep(1)
+            GPIO.output(12, 0)
     except Exception as err:
         print(err)
     finally:
