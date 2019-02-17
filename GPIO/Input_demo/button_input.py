f2e72bfa73bc74a66dee8a1d6ace94fa09b78109
Kwan <allenxian@live.com>
2019-02-17 15:55:31 +0800

    Initial commit

--- GPIO/Input_demo/button_input.py
+++ GPIO/Input_demo/button_input.py
@@ -17,12 +17,16 @@ def gpio_init():
 if __name__ == '__main__':
     try:
         gpio_init()
+        print("Press Ctrl + C to stop")
+        # 轮询方式检测GPIO电平
         while True:
             if GPIO.input(18) == GPIO.HIGH:
                 GPIO.output(16, 1)
             else:
                 GPIO.output(16, 0)
             sleep(0.1)
+    except KeyboardInterrupt:
+        print("stop")
     except Exception as err:
         print(err)
     finally:
