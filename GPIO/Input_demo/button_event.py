d01b1c2d9fdaccd550e7ae0251f6048892904b8c
Kwan <allenxian@live.com>
2019-02-17 16:40:11 +0800

    Initial commit

--- GPIO/Input_demo/button_event.py
+++ GPIO/Input_demo/button_event.py
@@ -7,6 +7,7 @@ from time import sleep
 
 # 事件触发方式等待
 def button_event(channel):
+    print("event")
     if GPIO.input(channel) == GPIO.HIGH:
         GPIO.output(16, 1)
     else:
@@ -19,7 +20,7 @@ def gpio_init():
     # 这个接口接到LED的正极，LED的负极接到GND即可
     GPIO.setup(16, GPIO.OUT)
     GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(18, GPIO.RISING, callback=button_event, bouncetime=200)
+    GPIO.add_event_detect(18, GPIO.RISING, button_event, bouncetime=200)
     print("GPIO init success!")
 
 
