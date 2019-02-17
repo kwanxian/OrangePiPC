71f55b1bdec2bf684e32ec66d3f2284a8a33e68d
Kwan <allenxian@live.com>
2019-02-17 14:56:37 +0800

    Initial commit

--- GPIO/LED_demo/one_led.py
+++ GPIO/LED_demo/one_led.py
@@ -8,15 +8,16 @@ from time import sleep
 def gpio_init():
     GPIO.setboard(GPIO.PCPCPLUS)
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(12, GPIO.OUT)
+    GPIO.setup(16, GPIO.OUT)
     print("GPIO init success!")
 
 
 if __name__ == '__main__':
     try:
         gpio_init()
-        GPIO.output(12, 1)
+        GPIO.output(16, 1)
         sleep(5)
+        GPIO.output(16, 0)
         # while True:
         #     GPIO.output(12, 1)
         #     sleep(1)
