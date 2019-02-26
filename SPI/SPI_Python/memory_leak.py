9f24c7efeccc6d282292959c78c2835cb8784b4a
kwan <allenxian@live.com>
2019-02-26 21:28:20 +0800

    Initial commit

--- SPI/SPI_Python/memory_leak.py
+++ SPI/SPI_Python/memory_leak.py
@@ -5,14 +5,10 @@ def transact():
 	data_out = (0xFF,0x00,0xFA)
 	# Open file descriptor for 
 	# spi device 0 using the CE0 pin for chip select
-	device_0 =  spi.openSPI(device="/dev/spidev0.0",
-							mode=0,
-							speed=1000000)
-							
+	device_0 =  spi.openSPI(device="/dev/spidev0.0", mode=0, speed=1000000)
 	print("**")
 	print(device_0)
-
-    # This is not necessary, not just demonstrate loop-back
+	# This is not necessary, not just demonstrate loop-back
 	data_in = (0x00, 0x00, 0x00)
 	data_in = spi.transfer(device_0, data_out)
 	print("Received from device 0:")
